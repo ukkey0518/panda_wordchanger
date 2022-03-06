6bdde706180498235b075ee993ce410e7fcfacd1
r.ukita <r.ukita@ukihome.com>
2022-03-06 11:16:23 +0900

    Fix Flavor

--- lib/app/app_flavor.dart
+++ lib/app/app_flavor.dart
@@ -1,6 +1,10 @@
 mixin AppFlavor {
   static late final AppFlavorMode mode;
 
+  static bool get isDev => mode.isDev;
+
+  static bool get isPrd => mode.isPrd;
+
   @override
   String toString() {
     return 'AppFlavor{mode: $mode}';
@@ -15,6 +19,10 @@ enum AppFlavorMode {
 extension AppFlavorModeEx on AppFlavorMode {
   static const _devName = 'dev';
   static const _prdName = 'prd';
+
+  bool get isDev => this == AppFlavorMode.dev;
+
+  bool get isPrd => this == AppFlavorMode.prd;
 
   String get name {
     switch (this) {
--- lib/app/main_prd.dart
+++ lib/app/main_prd.dart
@@ -1,11 +1,10 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
-
 // Project imports:
 import 'package:panda_wordchanger/app/app.dart';
 import 'package:panda_wordchanger/app/app_flavor.dart';
 
 void main() {
-  AppFlavor.mode = AppFlavorMode.dev;
+  AppFlavor.mode = AppFlavorMode.prd;
   runApp(const App());
 }
